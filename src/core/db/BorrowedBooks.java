206c17de1ded44e9829dfc8a2c382aec22dcf799
unknown <a.utegenova@student.inha.uz>
2019-12-20 01:58:48 +0500

    feat: create & delete & fetch from DB

--- src/core/db/BorrowedBooks.java
+++ src/core/db/BorrowedBooks.java
@@ -1,4 +1,27 @@
 package core.db;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class BorrowedBooks {
+    public void createBorrowedBooks(int ISBN,String ID) {
+        Database db = Database.getInstance();
+
+        try {
+            PreparedStatement psInsert = db.connection.prepareStatement("INSERT INTO BorrowedBooks(ISBN, ID) VALUES (?,?)");
+            psInsert.setInt(1, ISBN);
+            psInsert.setString(2, ID);
+          ;
+
+            psInsert.executeUpdate();
+            psInsert.close();
+            System.out.println("Inserted: " + ISBN + ", " + ID);
+        } catch (SQLException e) {
+            Database.printSQLException(e);
+        }
+    }
+
+
+
 }

63d526220798f26a53fcfc3901e5d53958848086
ismailovasevilya <sevilya.ismailova1999@gmail.com>
2019-12-19 21:20:37 +0300

    chore: add student page

--- src/ui/Main.java
+++ src/ui/Main.java
@@ -10,7 +10,9 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("views/login.fxml"));
+        //Parent root = FXMLLoader.load(getClass().getResource("views/login.fxml"));
+        //Parent root = FXMLLoader.load(getClass().getResource("Item.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("StudentPage.fxml"));
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();

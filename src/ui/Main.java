7ca4eeed70e315eee3aff18c6d654cc84c2ea31a
Aquarelle <maxbubakomilova@gmail.com>
2019-12-20 01:35:53 +0500

    feat:add student list

--- src/ui/Main.java
+++ src/ui/Main.java
@@ -5,14 +5,26 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 public class Main extends Application {
+    private double x,y;
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("views/login.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        Parent root = FXMLLoader.load(getClass().getResource("views/AdminPage.fxml"));
+        primaryStage.setScene(new Scene(root));
+        primaryStage.initStyle(StageStyle.DECORATED);
+
+        root.setOnMousePressed(event -> {
+            x = event.getSceneX();
+            y = event.getSceneY();
+        });
+        root.setOnMouseDragged(event -> {
+            primaryStage.setX(event.getScreenX()-x);
+            primaryStage.setY(event.getSceneY()-y);
+
+        });
         primaryStage.show();
     }
 
--- src/ui/controllers/Controller.java
+++ src/ui/controllers/Controller.java
@@ -1,7 +1,33 @@
 package ui.controllers;
 
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.layout.VBox;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 import javafx.scene.control.Button;
 
-public class Controller {
+public class Controller implements Initializable {
 
+        @FXML
+        private VBox pnItems;
+
+
+    @Override
+        public void initialize(URL location, ResourceBundle resources){
+        Node [] nodes = new Node[10];
+        for(int i = 0; i < nodes.length; i++){
+            try {
+                nodes[i] = FXMLLoader.load(getClass().getResource("Item.fxml"));
+                pnItems.getChildren().add(nodes[i]);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
+    }
 }

6e6fa7940e49badb799ffa29ea4643f7661ab3ff
ismailovasevilya <sevilya.ismailova1999@gmail.com>
2019-12-22 01:49:33 +0300

    feat: add login page

--- src/ui/Main.java
+++ src/ui/Main.java
@@ -1,20 +1,45 @@
 package ui;
 
 import javafx.application.Application;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
+import javafx.stage.Stage;
+
+
+import java.awt.*;
 
 public class Main extends Application {
 
+
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("views/login.fxml"));
-        primaryStage.setTitle("Hello World");
+        Parent root = FXMLLoader.load(getClass().getResource("Login.fxml"));
+        primaryStage.setTitle("Library System Management");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
+
     }
+
 
 
     public static void main(String[] args) {
--- src/ui/controllers/Controller.java
+++ src/ui/controllers/Controller.java
@@ -1,7 +1,49 @@
 package ui.controllers;
 
-import javafx.scene.control.Button;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.image.Image;
 
-public class Controller {
+import java.net.URL;
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.ResourceBundle;
+
+import javafx.scene.control.TextField;
+import javafx.scene.paint.ImagePattern;
+import javafx.scene.shape.Circle;
+
+
+public class Controller implements Initializable{
+
+
+    @FXML
+    private Circle myCircle;
+    @FXML
+    private TextField studentName;
+    @FXML
+    private TextField studentId;
+
+
+
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb){
+       // myCircle.setStroke(Color.BLACK);
+        Image im = new Image("ui/views/icons/bober.jpg" ,false);
+        myCircle.setFill(new ImagePattern(im));
+        studentName.setText("Here will be name");
+        studentId.setText("Here will be ID");
+
+    }
+
+
+
 
 }

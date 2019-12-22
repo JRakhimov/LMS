76ed582afbec58fbaf59d6982fdea4932571613a
Aquarelle <maxbubakomilova@gmail.com>
2019-12-22 15:04:55 +0500

    chore: admin page students & librarians

--- src/ui/Main.java
+++ src/ui/Main.java
@@ -9,6 +9,7 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 import java.sql.SQLException;
 
@@ -17,26 +18,28 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("views/loginPage.fxml"));
+
+        Parent root = FXMLLoader.load(getClass().getResource("views/AdminPage_students.fxml"));
         primaryStage.setTitle("Library System Management");
-        primaryStage.setScene(new Scene(root, 1200, 800));
+        primaryStage.setScene(new Scene(root,1200,800));
+        primaryStage.initStyle(StageStyle.UNDECORATED);
 
         primaryStage.show();
     }
 
     public static void main(String[] args) {
-        Database.init("jdbc:derby://localhost:3303/LMS", "root", "root");
-
-        try {
+//        Database.init("jdbc:derby://localhost:3303/LMS", "root", "root");
+//
+//        try {
 //            BorrowedBooks.createBorrowedBooks(3, 3);
-
-            Book[] books = BorrowedBooks.fetchBorrowedBooksByStudent(3);
-
-            System.out.println(books.length);
-            System.out.println(books[0]);
-        } catch (SQLException e) {
-            Database.printSQLException(e);
-        }
+//
+//            Book[] books = BorrowedBooks.fetchBorrowedBooksByStudent(3);
+//
+//            System.out.println(books.length);
+//            System.out.println(books[0]);
+//        } catch (SQLException e) {
+//            Database.printSQLException(e);
+//        }
 
        launch(args);
     }
